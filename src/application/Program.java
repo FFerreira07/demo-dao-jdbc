914c0a5d9f7639fe79cd4b562077cc518c8fe353
Felipe Ferreira <felipeferreira2412@gmail.com>
2023-03-31 18:28:33 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
         Department department = new Department(1, "Books");
 
         Seller seller = new Seller(10, "Felipe", "felipe@gmail.com", LocalDate.now(), 5000.00, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
