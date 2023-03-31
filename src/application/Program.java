52dd523146fa010d29e79efb86aa389f2ca9e542
Felipe Ferreira <felipeferreira2412@gmail.com>
2023-03-31 18:03:46 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,17 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Books");
-        System.out.println(department);
+
+        Seller seller = new Seller(10, "Felipe", "felipe@gmail.com", LocalDate.now(), 5000.00, department);
+
+        System.out.println(seller);
     }
 }
