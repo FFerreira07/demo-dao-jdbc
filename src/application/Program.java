6c10808c4aec8d1aba94b6b0dc2bd88e89411066
Felipe Ferreira <felipeferreira2412@gmail.com>
2023-03-31 20:54:21 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,15 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
 
-        Department department = new Department(1, "Books");
-
-        Seller seller = new Seller(10, "Felipe", "felipe@gmail.com", LocalDate.now(), 5000.00, department);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
