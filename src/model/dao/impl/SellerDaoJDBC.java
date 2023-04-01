46a14a2d3ad34fb01f73d0bcbfb9b754bc43d5ee
Felipe Ferreira <felipeferreira2412@gmail.com>
2023-03-31 23:28:03 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -94,4 +97,44 @@ public class SellerDaoJDBC implements SellerDao {
         throw new UnsupportedOperationException("Unimplemented method 'findAll'");
     }
 
+    @Override
+    public List<Seller> findByDepartment(Department dep) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+
+            st = conn.prepareStatement(
+                    "SELECT seller.*, department.Name as DepName " +
+                            "FROM seller INNER JOIN department " +
+                            "ON seller.DepartmentId = department.Id " +
+                            "WHERE department.Id = ? " +
+                            "ORDER BY seller.Name");
+
+            st.setInt(1, dep.getId());
+
+            rs = st.executeQuery();
+            List<Seller> sellers = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+                Department department = map.get(rs.getInt("DepartmentId"));
+
+                if (department == null) {
+                    department = instantiateDepartment(rs);
+                    map.put(department.getId(), department);
+                }
+
+                Seller seller = instantiateSeller(rs, department);
+                sellers.add(seller);
+            }
+            return sellers;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
 }
